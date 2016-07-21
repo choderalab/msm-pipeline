b5cc93f3d2a8ba41c7ae66451f6fcc5c8a84bf52
Josh Fass <maxentile@users.noreply.github.com>
2016-07-21 07:46:10 -0400

    Update contact_features.py
    Add whitespace

--- contact_features.py
+++ contact_features.py
@@ -36,22 +36,22 @@ def find_respairs_that_changed(fnames,
     '''
     distances = []
     for fname in fnames:
-        traj = md.load(fname,stride=stride)
-        pairwise_distances,residue_pairs = md.compute_contacts(traj,scheme=scheme)
+        traj = md.load(fname, stride = stride)
+        pairwise_distances,residue_pairs = md.compute_contacts(traj, scheme = scheme)
         distances.append(pairwise_distances)
     distances = np.vstack(distances)
 
     # identify contacts that change by counting how many times the distances were
     # greater than and less than the threshold
-    num_times_greater_than = (distances>threshold).sum(0)
-    num_times_less_than = (distances<threshold).sum(0)
+    num_times_greater_than = (distances > threshold).sum(0)
+    num_times_less_than = (distances < threshold).sum(0)
     changed = (num_times_greater_than > 0) * (num_times_less_than > 0)
     print("Number of contacts that changed: {0}".format(changed.sum()))
     print("Total number of possible contacts: {0}".format(len(residue_pairs)))
 
     if len(changed) > max_respairs:
-        n_diff = np.min(np.vstack((num_times_less_than,num_times_greater_than)),0)
-        indices = sorted(np.arange(len(n_diff)),key=lambda i:-n_diff[i])
+        n_diff = np.min(np.vstack((num_times_less_than, num_times_greater_than)), 0)
+        indices = sorted(np.arange(len(n_diff)), key = lambda i: -n_diff[i])
         changed = indices[:max_respairs]
 
     # now turn this bitmask into a list of relevant residue pairs
