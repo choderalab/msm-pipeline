9e671843ef8742080c5c3b3da2163c9ba41c150b
Josh Fass <maxentile@users.noreply.github.com>
2016-08-15 15:52:30 -0400

    Fix url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import versioneer
 setup(name='msmpipeline',
       version=versioneer.get_version(),
       description='Sensible defaults for MSMs',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/choderalab/msm-pipeline/',
       author='Sonya Hanson, Steven Albanese, Josh Fass',
       author_email='{sonya.hanson, steven.albanese, josh.fass}@choderalab.org',
       license='MIT',
