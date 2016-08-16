3be8bf24d3954bfc1ae35a0c93e5d745d82ff2a2
Josh Fass <jf694@cornell.edu>
2016-08-16 11:22:12 -0400

    Avoid a time-consuming download
    - Replace MetEnkaphalin example with random data
    - Also apparently trim extraneous whitespace. Thank you, Atom!

--- msmpipeline/test_report_generation.py
+++ msmpipeline/test_report_generation.py
@@ -1,22 +1,22 @@
+import numpy as np
+import numpy.random as npr
+
 def generate_test_instance():
     '''
     Fetches example data from `msmbuilder.example_datasets`, then clusters w.r.t.
     a tICA-derived kinetic distance, and returns results.
-    
+
     Returns
     -------
     dtrajs : list of int-arrays
-    
+
     tica : pyemma tICA object
-    
+
     tica_output : list of numpy.ndarrays
-    
+
     msm : pyemma MSM object
     '''
-    from msmbuilder.example_datasets import MetEnkephalin
-    trajs = MetEnkephalin().get().trajectories
-    from msmbuilder.featurizer import DihedralFeaturizer
-    X = DihedralFeaturizer().fit_transform(trajs)
+    X = [npr.randn(1000,100) for _ in range(10)]
     import pyemma
     tica = pyemma.coordinates.tica(X)
     tica_output = tica.get_output()
@@ -29,7 +29,7 @@ def generate_test_instance():
 
 def main():
     dtrajs, tica, tica_output, msm = generate_test_instance()
-    
+
     from generate_report import make_plots, compute_its
     compute_its(dtrajs, project_name = 'test')
     make_plots(dtrajs, tica, tica_output, msm, project_name = 'test')
