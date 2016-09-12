6acca615999c6907438ba40012b6ff3e2357e4e5
Josh Fass <maxentile@users.noreply.github.com>
2016-09-12 17:24:06 -0400

    Parallelize over fnames

--- msmpipeline/contact_features.py
+++ msmpipeline/contact_features.py
@@ -1,11 +1,37 @@
+from multiprocessing import Pool
+from functools import partial
 import mdtraj as md
 import numpy as np
+
+def get_distances(fname, scheme, stride):
+    '''
+    Function callable by a multiprocessing Pool
+    
+    Parameters
+    ----------
+    fname : string
+        filename of trajectory
+    scheme : string
+        'ca' or 'closest' or 'closest-heavy'
+    stride : int
+        thinning factor: only look at every `stride`th frame
+        
+    Returns
+    -------
+    pairwise_distances : numpy array
+    
+    residue_pairs : list of tuples
+    '''
+    traj = md.load(fname, stride = stride)
+    pairwise_distances,residue_pairs = md.compute_contacts(traj, scheme = scheme)
+    return pairwise_distances, residue_pairs
 
 def find_respairs_that_changed(fnames,
                                scheme = 'ca',    # or 'closest' or 'closest-heavy'
                                threshold = 0.4,
                                stride = 100,
-                               max_respairs = 1000):
+                               max_respairs = 1000,
+                               num_threads = 12):
     '''
     Finds all the residue pairs that crossed `threshold` at least once in a strided subset of 
     trajectory data. If that number of features is still too high, just take the top `max_respairs`
@@ -28,18 +54,21 @@ def find_respairs_that_changed(fnames,
         
     max_respairs : int
         maximum number of features to return
+        
+    num_threads : int
+        number of threads to use 
     
     Returns
     -------
     respairs_that_changed : list of tuples
         each element of the list is a length-2 tuple
     '''
-    distances = []
-    for fname in fnames:
-        traj = md.load(fname, stride = stride)
-        pairwise_distances,residue_pairs = md.compute_contacts(traj, scheme = scheme)
-        distances.append(pairwise_distances)
-    distances = np.vstack(distances)
+    # parallelize over fnames
+    pool = Pool(num_threads)
+    get_distances_ = partial(get_distances, scheme=scheme, stride=stride)
+    distances = pool.map(get_distances_, fnames)
+    residue_pairs = distances[0][1]
+    distances = np.vstack([d[0] for d in distances])
 
     # identify contacts that change by counting how many times the distances were
     # greater than and less than the threshold
